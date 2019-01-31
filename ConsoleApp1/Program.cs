60acf64e3d49a18ae4c664bd51e064eb76954f4a
visualstudio <visualstudio@230-PC24>
2019-01-31 14:17:47 -0500

    Commit 2:02 PM

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Program.cs
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
+
+    class Village
+
+    {
+        public static int numberOfVillages = 0;
+        public Village nextVillage;
+        public Village previousVillage;
+        public string VillageName;
+        public bool isAstrildeHere = false;
+
+        private Village()
+        {
+            Village.numberOfVillages++;
+        }
+    }
+
+    class Countryside
+    {
+        public Village Maple;
+        public Village Toronto;
+        public Village Ajax;
+
+        public void MapInitializer()
+        {
+            Maple = new Village();
+            Maple.VillageName = "Maple";
+            Maple.previousVillage = null;
+            Maple.nextVillage = Toronto;
+            Toronto = new Village();
+            Toronto.VillageName = "Toronto";
+            Toronto.previousVillage = Maple;
+            Toronto.nextVillage = Ajax;
+            Ajax = new Village();
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.isAstrildeHere = true;
+        }
+
+        public void LookForAstr
+    }
+
 }
+
+
+
